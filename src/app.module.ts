85d122eb4aec981d12cfe48f5f483c0f20218190
TranVanLuong <tranvanluong032020@gmail.com>
2025-10-08 18:51:58 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,3 @@
-// import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { ConfigModule, ConfigService } from '@nestjs/config';
-// import { UsersModule } from './users/users.module';
-// import { AllCodesModule } from './modules/all-codes/all-codes.module';
-// import { User } from 'src/users/entities/user.entity';
-// import { AuthModule } from './modules/auth/auth.module';
-// import { DatabasesModule } from './databases/databases.module';
-// import { AllCode } from 'src/modules/all-codes/entities/all-code.entity';
-// import { PermissionsModule } from './permissions/permissions.module';
-// import { RolesModule } from './modules/roles/roles.module';
-// import { Permission } from 'src/permissions/entities/permission.entity';
-// import { RolePermissionModule } from './modules/role_permission/role_permission.module';
-// import { Role } from 'src/modules/roles/entities/role.entity';
-
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -26,11 +9,11 @@ import { AllCode } from 'src/modules/all-codes/entities/all-code.entity';
 import { AuthModule } from 'src/modules/auth/auth.module';
 import { Permission } from 'src/modules/permissions/entities/permission.entity';
 import { PermissionsModule } from 'src/modules/permissions/permissions.module';
-import { RolePermissionModule } from 'src/modules/role_permission/role_permission.module';
 import { Role } from 'src/modules/roles/entities/role.entity';
 import { RolesModule } from 'src/modules/roles/roles.module';
 import { User } from 'src/modules/users/entities/user.entity';
 import { UsersModule } from 'src/modules/users/users.module';
+import { RolePermissionModule } from './modules/role_permission/role_permission.module';
 
 @Module({
   imports: [
