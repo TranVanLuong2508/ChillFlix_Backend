6c004d01d4b267ef61e9a14c7c491b3c0e890c1a
TranVanLuong <tranvanluong032020@gmail.com>
2025-10-29 16:38:04 +0700

    update vnpay

--- src/modules/payments/dto/create-payment.dto.ts
+++ src/modules/payments/dto/create-payment.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePaymentDto {}
+import { IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
+
+export class CreatePaymentDto {
+  @IsNotEmpty()
+  @IsNumber({}, { message: 'amout mus be a number' })
+  @Min(1)
+  amount: number; // số tiền thanh toán, VNPay yêu cầu nhân 100 trong backend
+
+  @IsNotEmpty()
+  @IsString()
+  orderId: string; // mã đơn hàng do bạn tạo
+}
--- src/modules/payments/payments.service.ts
+++ src/modules/payments/payments.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Req, Res } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, Req, Res } from '@nestjs/common';
 import { CreatePaymentDto } from './dto/create-payment.dto';
 import { UpdatePaymentDto } from './dto/update-payment.dto';
 import moment from 'moment';
@@ -32,99 +32,120 @@ export class PaymentsService {
   }
 
   createPaymentUrl(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
-    let ipAddr =
-      req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || '127.0.0.1';
+    try {
+      //ip khachs hàng
+      let ipAddr =
+        req.headers['x-forwarded-for'] || req.connection?.remoteAddress || req.socket?.remoteAddress || '127.0.0.1';
 
-    // if ipAddr is Array, get first element
-    if (Array.isArray(ipAddr)) {
-      ipAddr = ipAddr[0];
+      if (Array.isArray(ipAddr)) {
+        ipAddr = ipAddr[0];
+      }
+
+      if (typeof ipAddr === 'string') {
+        ipAddr = ipAddr.replace('::ffff:', '');
+        if (ipAddr === '::1') ipAddr = '127.0.0.1';
+      }
+
+      let currentDate = new Date();
+      let createDate = moment(currentDate).format('YYYYMMDDHHmmss');
+      let tmnCode = this.configService.get<string>('VNP_TMN_CODE');
+      let secretKey = this.configService.get<string>('VNP_HASH_SECRET');
+      let returnUrl = this.configService.get<string>('VNP_RETURN_URL');
+      let orderId = moment(currentDate).format('DDHHmmss');
+      let amount = 49000;
+      let locale = 'vn';
+      let currCode = 'VND';
+      let orederInfor = 'Thanh toan qua VNPay cho don hang dang ky goi vip tren ChillFlix voi ma GD: ' + orderId;
+      let orderType = 190003;
+
+      let vnp_Params: any = {
+        vnp_Version: '2.1.0',
+        vnp_Command: 'pay',
+        vnp_TmnCode: tmnCode,
+        vnp_Locale: locale,
+        vnp_CurrCode: currCode,
+        vnp_TxnRef: orderId + '-' + moment(new Date()).format('HHmmss'),
+        vnp_OrderInfo: orederInfor,
+        vnp_OrderType: orderType,
+        vnp_Amount: amount * 100, // VNPay yêu cầu đơn vị = đồng × 100
+        vnp_ReturnUrl: returnUrl,
+        vnp_IpAddr: ipAddr,
+        vnp_CreateDate: createDate,
+      };
+
+      // if (bankCode !== null && bankCode !== '') {
+      //   vnp_Params['vnp_BankCode'] = bankCode;
+      // }
+      // vnp_Params['vnp_BankCode'] = 'NCB';
+
+      vnp_Params = sortObject(vnp_Params);
+      let signData = querystring.stringify(vnp_Params, { encode: false }); //convert vnp_Params to qwuery string
+      let hmac = crypto.createHmac('sha512', secretKey);
+
+      let signed = hmac.update(new Buffer(signData, 'utf-8')).digest('hex');
+      vnp_Params['vnp_SecureHash'] = signed;
+
+      let vnpUrl = this.configService.get<string>('VNP_URL');
+      vnpUrl += '?' + querystring.stringify(vnp_Params, { encode: false });
+
+      return {
+        EC: 1,
+        EM: 'Create VNPAY payment URL success',
+        metadata: {
+          redirectUrl: vnpUrl,
+        },
+      };
+    } catch (error) {
+      console.error('Error in Create VNPAY payment URL:', error.message);
+      throw new InternalServerErrorException({
+        EC: 0,
+        EM: 'Error from create payment url service',
+      });
     }
-
-    if (typeof ipAddr === 'string') {
-      ipAddr = ipAddr.replace('::ffff:', '');
-      if (ipAddr === '::1') ipAddr = '127.0.0.1';
-    }
-
-    let currentDate = new Date();
-    let createDate = moment(currentDate).format('YYYYMMDDHHmmss');
-    let tmnCode = this.configService.get<string>('VNP_TMN_CODE');
-    let secretKey = this.configService.get<string>('VNP_HASH_SECRET');
-    let vnpUrl = this.configService.get<string>('VNP_URL');
-    let returnUrl = this.configService.get<string>('VNP_RETURN_URL');
-    let orderId = moment(currentDate).format('DDHHmmss');
-    let amount = 49000;
-    let locale = 'vn';
-    let currCode = 'VND';
-
-    let vnp_Params = {};
-    vnp_Params['vnp_Version'] = '2.1.0';
-    vnp_Params['vnp_Command'] = 'pay';
-    vnp_Params['vnp_TmnCode'] = tmnCode;
-    vnp_Params['vnp_Locale'] = locale;
-    vnp_Params['vnp_CurrCode'] = currCode;
-    vnp_Params['vnp_TxnRef'] = Date.now().toString(); //tes demo
-    vnp_Params['vnp_OrderInfo'] = 'Thanh toan cho ma GD:' + orderId;
-    vnp_Params['vnp_OrderType'] = 'other';
-    vnp_Params['vnp_Amount'] = amount * 100; // VNPay yêu cầu đơn vị = đồng × 100
-    vnp_Params['vnp_ReturnUrl'] = returnUrl;
-    vnp_Params['vnp_IpAddr'] = ipAddr;
-    vnp_Params['vnp_CreateDate'] = createDate;
-
-    // if (bankCode !== null && bankCode !== '') {
-    //   vnp_Params['vnp_BankCode'] = bankCode;
-    // }
-    // vnp_Params['vnp_BankCode'] = 'NCB';
-
-    vnp_Params = sortObject(vnp_Params);
-    let signData = querystring.stringify(vnp_Params, { encode: false });
-    let hmac = crypto.createHmac('sha512', secretKey);
-    let signed = hmac.update(new Buffer(signData, 'utf-8')).digest('hex');
-    vnp_Params['vnp_SecureHash'] = signed;
-    vnpUrl += '?' + querystring.stringify(vnp_Params, { encode: false });
-
-    return {
-      vnpUrl,
-    };
   }
 
   verifyReturn(@Req() req: Request, @Res() res: Response) {
-    // logic:
-    // Xóa vnp_SecureHash và vnp_SecureHashType
+    try {
+      let vnp_Params = req.query;
+      let secureHash = vnp_Params['vnp_SecureHash'];
 
-    // Tự tính lại chữ ký hash
+      delete vnp_Params['vnp_SecureHash'];
+      delete vnp_Params['vnp_SecureHashType'];
 
-    // So sánh với hash VNPay gửi
+      vnp_Params = sortObject(vnp_Params);
 
-    // Nếu trùng → giao dịch hợp lệ.
-    let vnp_Params = req.query;
+      let tmnCode = this.configService.get<string>('VNP_TMN_CODE');
+      let secretKey = this.configService.get<string>('VNP_HASH_SECRET');
 
-    let secureHash = vnp_Params['vnp_SecureHash'];
+      let signData = querystring.stringify(vnp_Params, { encode: false });
 
-    delete vnp_Params['vnp_SecureHash'];
-    delete vnp_Params['vnp_SecureHashType'];
+      let hmac = crypto.createHmac('sha512', secretKey);
+      let signed = hmac.update(new Buffer(signData, 'utf-8')).digest('hex');
 
-    vnp_Params = sortObject(vnp_Params);
+      const frontendURL = this.configService.get<string>('FRONTEND_URL');
+      if (secureHash === signed) {
+        //add logic
+        console.log('chekc', vnp_Params);
+        res.redirect(`${frontendURL}`);
 
-    let tmnCode = this.configService.get<string>('VNP_TMN_CODE');
-    let secretKey = this.configService.get<string>('VNP_HASH_SECRET');
-
-    let signData = querystring.stringify(vnp_Params, { encode: false });
-
-    let hmac = crypto.createHmac('sha512', secretKey);
-    let signed = hmac.update(new Buffer(signData, 'utf-8')).digest('hex');
-
-    if (secureHash === signed) {
-      //Kiem tra xem du lieu trong db co hop le hay khong va thong bao ket qua
-      console.log('chekc', vnp_Params);
-      return res.status(200).json({
-        message: 'success',
-        code: vnp_Params['vnp_ResponseCode'],
-        data: vnp_Params,
-      });
-    } else {
-      return res.status(200).json({
-        message: 'Checksum failed',
-        code: '97',
+        // return res.status(200).json({
+        //   message: 'success',
+        //   code: vnp_Params['vnp_ResponseCode'],
+        //   data: vnp_Params,
+        // });
+        // res.render("success", { code: vnp_Params["vnp_ResponseCode"] });
+      } else {
+        // res.redirect('frontend link you want to redirect')
+        return res.status(200).json({
+          message: 'Checksum failed',
+          code: '97',
+        });
+      }
+    } catch (error) {
+      console.error('Error in verifyReturn VNPAY:', error.message);
+      throw new InternalServerErrorException({
+        EC: 0,
+        EM: 'Error from verifyReturn service',
       });
     }
   }
