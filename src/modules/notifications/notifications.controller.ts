7a372202c54135eada3681b13f6cccadd3942631
TranVanLuong <tranvanluong032020@gmail.com>
2025-11-19 20:56:39 +0700

    update not

--- src/modules/notifications/notifications.controller.ts
+++ src/modules/notifications/notifications.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Post, Patch, Delete, Param, Query, UseGuards } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
-import { Public, User } from 'src/decorators/customize';
+import { Public, SkipCheckPermission, User } from 'src/decorators/customize';
 import type { IUser } from '../users/interface/user.interface';
 
 @Controller('notifications')
@@ -8,26 +8,31 @@ export class NotificationsController {
   constructor(private readonly notificationsService: NotificationsService) {}
 
   @Get()
+  @SkipCheckPermission()
   getNotifications(@User() user: IUser, @Query() query: any) {
     return this.notificationsService.getNotifications(user, query);
   }
 
   @Get('unread-count')
+  @SkipCheckPermission()
   getUnreadCount(@User() user: IUser) {
     return this.notificationsService.getUnreadCount(user.userId);
   }
 
   @Patch(':id/read')
+  @SkipCheckPermission()
   markAsRead(@Param('id') id: string, @User() user: IUser) {
     return this.notificationsService.markAsRead(parseInt(id), user);
   }
 
   @Patch('read-all')
+  @SkipCheckPermission()
   markAllAsRead(@User() user: IUser) {
     return this.notificationsService.markAllAsRead(user);
   }
 
   @Delete(':id')
+  @SkipCheckPermission()
   deleteNotification(@Param('id') id: string, @User() user: IUser) {
     return this.notificationsService.deleteNotification(parseInt(id), user);
   }
